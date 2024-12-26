63d8ed719c357e3ef8aa012f9cb6da6fd6986a29
Emre Öztürk <ozturkemre@mail.com>
2024-12-26 13:41:34 +0300

    Text to Speech Completed!

---
+++ main.py
@@ -0,0 +1,4 @@
+import pyttsx3
+kingTheoden = pyttsx3.init()
+kingTheoden.say("Arise, arise, Riders of Théoden.Fell deeds awake: fire and slaughter.Spear shall be shaken, shield be splintered,a sword-day, a red day, ere the sun rises!Ride now, ride now! Ride to Gondor!")
+kingTheoden.runAndWait()
